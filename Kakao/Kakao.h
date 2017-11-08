54f2eb9bac97031697397ba59d5fc6e084df1635
Muukii <m@muukii.me>
2017-11-08 12:13:54 +0900

    Initial Commit

---
+++ Kakao/Kakao.h
@@ -0,0 +1,19 @@
+//
+//  Kakao.h
+//  Kakao
+//
+//  Created by muukii on 11/8/17.
+//  Copyright © 2017 muukii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kakao.
+FOUNDATION_EXPORT double KakaoVersionNumber;
+
+//! Project version string for Kakao.
+FOUNDATION_EXPORT const unsigned char KakaoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kakao/PublicHeader.h>
+
+
